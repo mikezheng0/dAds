85bc4ff9a1228f3c45a9cb87fc342cb34d4ce7d9
Mike Zheng <mike_zheng0@hotmail.com>
2018-03-04 19:46:47 -0500

    added media queries to ads

--- src/styles/card/index.js
+++ src/styles/card/index.js
@@ -1,24 +1,27 @@
 import styled from 'styled-components'
 
-const Card = styled.div`
+const Card = styled.div `
     background: #fff;
     border-radius: 10px;
     position: relative;
-    width: ${props => props.fullSize ? '100%' : '33%'};
+    width: ${props => props.fullSize ? 'auto' : '33%'};
     display: flex;
     flex-direction: column; 
     align-items: center;
-    padding: 8px 8px 48px 8px;
+    padding: 8px 8px 40px 8px;
+    @media (max-width: 700px) {
+      width: auto;
+    }
     
 `
-const CardImage = styled.img`
+const CardImage = styled.img `
     max-width: 100%;
     border-radius: 10px;
     max-height: 100%;
     overflow: hidden;
 `
 
-const HoverImageLink= styled.a`
+const HoverImageLink = styled.a `
     border-radius: 10px;
     position: absolute;
     top: 0;
@@ -28,21 +31,25 @@ const HoverImageLink= styled.a`
     &:hover {
       background-color:  rgba(13, 34, 68, 0.4);
     }
+`
 
-    &:hover > span {
+const HoverTitle = styled.span `
+    visibility: hidden;
+    font-size:20px;
+    position:absolute;
+    bottom: 8px;
+    left: 8px;
+    text-decoration: none;
+    color: white;
+    font-family: "Segoe UI";
+    ${HoverImageLink}:hover & {
       visibility: visible;
     }
 `
 
-const HoverTitle = styled.span`
-  visibility: hidden;
-  font-size:20px;
-  position:absolute;
-  bottom: 8px;
-  left: 8px;
-  text-decoration: none;
-  color: white
-  
-`
-
-export { Card, CardImage, HoverImageLink, HoverTitle }
+export {
+  Card,
+  CardImage,
+  HoverImageLink,
+  HoverTitle
+}
--- src/styles/layout.js
+++ src/styles/layout.js
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 const AdGrid = styled.div`
   display: flex;
   align-items: flex-start;
+  @media (max-width: 700px) {
+    flex-direction: column;
+  }
 `
 
 export {
